57ae8df3bb4e89c4aab67fde1b1a8cdffbddf93b
Aniketdasher <55886349+Aniketdasher@users.noreply.github.com>
2021-04-16 15:38:07 +0530

    Add files via upload

---
+++ behaviour.js
@@ -0,0 +1,11 @@
+new hoverEffect({
+
+    parent:document.querySelector('.distortion'),
+    intensity:0.7,
+    image1:'./image1.jpg',
+    image2:'./image2.jpg',
+    angle:Math.PI / 8,
+    displacementImage:'./heightMap.png'
+
+});
+
